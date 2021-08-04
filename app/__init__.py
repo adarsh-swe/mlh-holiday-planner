1a385bc4d4cee2107c0952b091d9154fa6e60fa2
PurpleUltralisk <uw.david.jiang@gmail.com>
2021-08-04 17:22:28 -0400

    initial template

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import (
+    Flask, render_template, request,
+)
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
