4d9dc02d599d115c7f0270b1653b73ca84f326a1
jhanavigera <jhanavigera@gmail.com>
2021-08-05 00:02:10 -0400

    initial database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,31 @@
+import os
 from flask import (
     Flask, render_template, request,
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from ddtrace import patch_all
 
-app = Flask(__name__)
+
+db = SQLAlchemy()
+patch_all()
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+def create_app():
+    """Construct the core application."""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+        user=os.getenv("POSTGRES_USER"),
+        passwd=os.getenv("POSTGRES_PASSWORD"),
+        host=os.getenv("POSTGRES_HOST"),
+        port=5432,
+        table=os.getenv("POSTGRES_DB"),
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        from . import routes  # Import routes
+        db.create_all()  # Create database tables for our data models
+        return app
---
+++ app/routes.py
@@ -0,0 +1,65 @@
+import os
+from flask import (
+    Flask, render_template, request,
+)
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import create_app
+from .models import User, db
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=("GET", "POST"))
+def register():
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif UserModel.query.filter_by(username=username).first() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a restister page
+    # return "Register Page not yet implemented", 501
+    return render_template("register.html")
+
+
+@app.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        user = UserModel.query.filter_by(username=username).first()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user.password, password):
+            error = "Incorrect password."
+
+        if error is None:
+            return "Login Successful", 200
+        else:
+            return error, 418
+
+    ## TODO: Return a login page
+    # return "Login Page not yet implemented", 501
+    return render_template("login.html")
+
+
