00dfc014be7313282852d6d3ce7dfcaa8a1255be
changyou <changyou1009@gmail.com>
2021-08-05 09:36:47 -0700

    updated index.html using figma design

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,34 @@
-<<<<<<< HEAD
-from flask import Flask
-=======
+# <<<<<<< HEAD
+# from flask import Flask
+# =======
+# from flask import (
+#     Flask, render_template, request,
+# )
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# >>>>>>> f0833b60b4a1562de186012a1481858b26e07501
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# <<<<<<< HEAD
+# def hello():
+#     return 'Hello, World!'
+# =======
+# def index():
+#     return render_template("index.html")
+# >>>>>>> f0833b60b4a1562de186012a1481858b26e07501
+
 from flask import (
     Flask, render_template, request,
 )
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
->>>>>>> f0833b60b4a1562de186012a1481858b26e07501
 
 app = Flask(__name__)
 
 
 @app.route('/')
-<<<<<<< HEAD
-def hello():
-    return 'Hello, World!'
-=======
 def index():
     return render_template("index.html")
->>>>>>> f0833b60b4a1562de186012a1481858b26e07501
